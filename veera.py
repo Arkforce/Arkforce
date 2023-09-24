dded16cb5c7680195ab42445360761638f3be14c
Arkforce <khodzhaev@gmail.com>
2023-09-23 23:28:45 -0500

    Message

---
+++ veera.py
@@ -0,0 +1,6 @@
+daily_calory = 1500
+running_dist = 4
+walking_steps = 1000ь                     
+
+monthly_goal = daily_calory>=1500 and running_dist>=4 or daily_calory>=1500 and walking_steps >=10000
+print("based on info provided above, veera's abiity to lose 10 pounds in a month is" , monthly_goal)
